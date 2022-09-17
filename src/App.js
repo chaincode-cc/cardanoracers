aa1a89ab6397a0eb2b7095f95673e41d4b66a12e
William <williamalvarez672@gmail.com>
2022-09-17 22:31:23 +0100

    set up placeholder webite

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './wheel.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <p className='title'>
+        CARDANO RACERS
+        </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          
+        Coming soon!
+        </p>
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
